5109c61d1192af8cd59473b6d64913a8da28f961
Artyom Aralov <artyom.aralov@gmail.com>
2018-10-12 22:29:33 +0300

    team tab

---
+++ SaleSystemGUI/src/main/java/ee/ut/math/tvt/salessystem/ui/controllers/TeamController.java
@@ -0,0 +1,13 @@
+package ee.ut.math.tvt.salessystem.ui.controllers;
+
+import ee.ut.math.tvt.salessystem.dao.SalesSystemDAO;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TeamController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+}
