083ad2ab16a05deb6846b7f21d07dc7942e67ba5
Artyom Aralov <artyom.aralov@gmail.com>
2018-10-13 14:54:12 +0300

    team tab modified

--- SaleSystemGUI/src/main/java/ee/ut/math/tvt/salessystem/ui/controllers/TeamController.java
+++ SaleSystemGUI/src/main/java/ee/ut/math/tvt/salessystem/ui/controllers/TeamController.java
@@ -1,13 +1,48 @@
 package ee.ut.math.tvt.salessystem.ui.controllers;
 
-import ee.ut.math.tvt.salessystem.dao.SalesSystemDAO;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URL;
+import java.util.Properties;
 import java.util.ResourceBundle;
 
 public class TeamController implements Initializable {
+    @FXML
+    private Label getTeamName;
+
+    @FXML
+    private ImageView getTeamImage;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+    }
+
+    @FXML
+    public void applicationData() {
+        try (FileReader reader = new FileReader("application.properties")) {
+            Properties properties = new Properties();
+            properties.load(reader);
+            String teamName = properties.getProperty("team_name");
+            getTeamName.setText(teamName);
+            String teamContactPerson = properties.getProperty("team_contact_person");
+            String teamMembers = properties.getProperty("team_members");
+
+            //team logo
+            File file = new File(properties.getProperty("team_logo"));
+            Image image = new Image(file.toURI().toString());
+            getTeamImage.setImage(image);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
